f1d9f9d827d8de99a5d9e7a6c2f0b76bb963bbfd
Cole Price <colevart@gmail.com>
2022-11-09 14:52:03 -0500

    supplier method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
-Product.create(name: "Tree", price: 100, image_url: "https://live.staticflickr.com/7060/27639474046_f241390840_m.jpg", description: "A woody plant that typlically has single stem or trunk growning to a considerable height and bearing lateral branches at some distance from the ground")
+# Product.create(name: "Tree", price: 100, image_url: "https://live.staticflickr.com/7060/27639474046_f241390840_m.jpg", description: "A woody plant that typlically has single stem or trunk growning to a considerable height and bearing lateral branches at some distance from the ground")
 
-Product.create(name: "Flower", price: 10, image_url: "https://live.staticflickr.com/2819/33995333900_f222e753af_m.jpg", description: "the seed-bearing part of a plant, consisting of reproductive organs (stamens and carpels) that are typically surrounded by a brightly colored corolla (petals) and a green calyx (sepals).")
+# Product.create(name: "Flower", price: 10, image_url: "https://live.staticflickr.com/2819/33995333900_f222e753af_m.jpg", description: "the seed-bearing part of a plant, consisting of reproductive organs (stamens and carpels) that are typically surrounded by a brightly colored corolla (petals) and a green calyx (sepals).")
 
-Product.create(name: "Pumpkin", price: 5, image_url: "https://live.staticflickr.com/3557/3390014088_fab9392bb2_n.jpg", description: "a large rounded orange-yellow fruit with a thick rind, edible flesh, and many seeds.")
+# Product.create(name: "Pumpkin", price: 5, image_url: "https://live.staticflickr.com/3557/3390014088_fab9392bb2_n.jpg", description: "a large rounded orange-yellow fruit with a thick rind, edible flesh, and many seeds.")
 
+Supplier.create(name: "Harrys Gardens", email: "hgardensgmail", phone_number: "267-999-0989")
+
+Supplier.create(name: "Mindys Garden Spot", email: "mgardens@gmail", phone_number: "808-245-4444")
